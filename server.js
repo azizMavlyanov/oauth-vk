e224ac74710d46367c4bf48cc534294dea151fd8
Aziz Mavlyanov <alex.mavlyanov95@gmail.com>
2020-01-11 00:16:43 +0300

    Changed route * with /

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static(publicPath));
 app.use(express.json());
 app.use(cors());
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   console.log(req);
   res.sendFile(path.join(publicPath, "index.html"));
 });
