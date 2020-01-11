e217cf4119e4a9b91d8f9dfe9f6d3e5537252e7f
Aziz Mavlyanov <alex.mavlyanov95@gmail.com>
2020-01-11 14:52:10 +0300

    Made some changes

--- server.js
+++ server.js
@@ -6,28 +6,34 @@ const app = express();
 const publicPath = path.join(__dirname, "public");
 const port = process.env.PORT || 3000;
 
+const clientId = "7277202";
+const clientSecret = "NHuM4xddzjBWIXEipLzp";
+const redirectUrl = "https://aziz-oauth-vk.herokuapp.com";
+
 // app.use(express.static(publicPath));
 app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-  if (!req.query.code) return res.send('<a href="/auth/vk">Log in via VK</a>');
-  console.log(req.query.code);
-  // res.sendFile(path.join(publicPath, "index.html"));
-  res.send(`code: ${req.query.code}`);
+  const code = req.query.code;
+  if (!code) return res.sendFile(path.join(publicPath, "index.html"));
+
+  console.log(code);
+
+  res.send(`code: ${code}`);
 });
 
-app.get("/auth/vk", async (req, res) => {
-  try {
-    const response = await axios.get(
-      "https://oauth.vk.com/authorize?client_id=7277202&scope=friends&redirect_uri=https://aziz-oauth-vk.herokuapp.com&response_type=code&v=5.103"
-    );
-    console.log(response.data);
-    res.send(response.data);
-  } catch (error) {
-    res.send({ error });
-  }
-});
+// app.get("/auth/vk", async (req, res) => {
+//   try {
+//     const response = await axios.get(
+//       `https://oauth.vk.com/authorize?client_id=${clientId}&scope=friends&redirect_uri=${redirectUrl}&response_type=code&v=5.103`
+//     );
+//     console.log(response.data);
+//     res.send(response.data);
+//   } catch (error) {
+//     res.send({ error });
+//   }
+// });
 
 app.listen(port, () => {
   console.log("Server is up!");
