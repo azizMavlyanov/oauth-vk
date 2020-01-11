4e24d0b5509f914c13246eceb7329331eac39f97
Aziz Mavlyanov <alex.mavlyanov95@gmail.com>
2020-01-11 15:39:47 +0300

    M

--- server.js
+++ server.js
@@ -23,12 +23,40 @@ app.get("/", async (req, res) => {
       `https://oauth.vk.com/access_token?client_id=7277202&client_secret=NHuM4xddzjBWIXEipLzp&code=${code}&redirect_uri=https://aziz-oauth-vk.herokuapp.com`
     );
     const { access_token, expires_in, user_id } = response.data;
+
+    const accessTokenResponse = await axios.get(
+      `https://api.vk.com/method/getProfiles?uid=${user_id}&access_token=${access_token}&v=5.103`
+    );
+
     console.log(response.data);
-    res.status(200).json({ access_token, expires_in, user_id });
+    res
+      .status(200)
+      .json({ access_token, expires_in, user_id, accessTokenResponse });
   } catch (error) {
     res.status(500).json({ error: "There is some error" });
   }
 });
+
+// const test = async () => {
+//   try {
+//     const codeResponse = await axios.get(
+//       `https://oauth.vk.com/access_token?client_id=7277202&client_secret=NHuM4xddzjBWIXEipLzp&code=b1171de254ef52d22c&redirect_uri=https://aziz-oauth-vk.herokuapp.com`
+//     );
+//     const { access_token, expires_in, user_id } = codeResponse.data;
+//     console.log(access_token);
+//     const accessTokenResponse = await axios.get(
+//       `https://api.vk.com/method/getProfiles?uid=${user_id}&access_token=${access_token}&v=5.103`
+//     );
+
+//     console.log(accessTokenResponse);
+//     // res.status(200).json({ access_token, expires_in, user_id });
+//   } catch (error) {
+//     console.log(error);
+//     // res.status(500).json({ error: "There is some error" });
+//   }
+// };
+
+// test();
 
 app.listen(port, () => {
   console.log("Server is up!");
